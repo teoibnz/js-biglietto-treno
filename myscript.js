ca16d7bc6aff2a350dee2c672a4c1979e869ef7b
Matteo Lopez <matteolopezbb@gmail.com>
2022-02-22 16:20:56 +0100

    creata condizione per under 18 e over 65

--- myscript.js
+++ myscript.js
@@ -5,19 +5,24 @@ const userAge = prompt("Quanti anni hai?");
 console.log("userAge");
 
 let priceBase = (0,21 * percorsoInKm);
-document.getElementById("full-price").innerHTML = priceBase;
 console.log(priceBase);
+
+if ((userAge > 18) || (userAge < 65)) {
+    document.getElementById('full-price').innerHTML = priceBase;
+};
 
 const discountTeen = ((priceBase * 20) / 100);
 console.log(discountTeen);
 
+if (userAge < 18) {
+    teenPrice = priceBase - discountTeen;
+    document.getElementById("teen-price").innerHTML = teenPrice;
+};
+
 const discountOld = ((priceBase * 40) / 100);
 console.log(discountOld);
 
-
-if (userAge < 18) {
-    priceBase - discountTeen;
-}
-else if (userAge > 65) {
-    priceBase - discountOld;
-}
+if (userAge > 65) {
+    oldPrice = priceBase - discountOld;
+    document.getElementById("old-price").innerHTML = oldPrice;
+};
