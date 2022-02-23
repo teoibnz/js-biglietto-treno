c50e100e94d710e72465ddd69b291d7f0789828e
Matteo Lopez <matteolopezbb@gmail.com>
2022-02-23 09:45:46 +0100

    refactoring js

--- myscript.js
+++ myscript.js
@@ -7,22 +7,26 @@ console.log("userAge");
 let priceBase = (0.21 * percorsoInKm);
 console.log(priceBase);
 
-if ((userAge > 18) || (userAge < 65)) {
-    document.getElementById('full-price').innerHTML = priceBase;
-};
 
 const discountTeen = ((priceBase * 20) / 100);
 console.log(discountTeen);
+
+const discountOld = ((priceBase * 40) / 100);
+console.log(discountOld);
+
 
 if (userAge < 18) {
     teenPrice = priceBase - discountTeen;
     document.getElementById("teen-price").innerHTML = teenPrice;
 };
 
-const discountOld = ((priceBase * 40) / 100);
-console.log(discountOld);
 
 if (userAge > 65) {
     oldPrice = priceBase - discountOld;
     document.getElementById("old-price").innerHTML = oldPrice;
 };
+
+if ((userAge > 18) && (userAge < 65)) {
+    document.getElementById('full-price').innerHTML = priceBase;
+};
+
